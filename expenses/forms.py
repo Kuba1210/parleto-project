3553b9ea975b91e5c01734ddce72ee6fbbfa6c80
Kuba <kuba.polakiewcz2002@gmail.com>
2023-03-09 01:10:20 +0100

    Committed all changes

--- expenses/forms.py
+++ expenses/forms.py
@@ -1,12 +1,22 @@
 from django import forms
-from .models import Expense
+from .models import Category, Expense
 
 
-class ExpenseSearchForm(forms.ModelForm):
-    class Meta:
-        model = Expense
-        fields = ('name',)
+class ExpenseSearchForm(forms.Form):
+    name = forms.CharField(required=False)
+    fromdate = forms.DateField(widget=forms.DateInput(attrs={"type": "date"}), required=False)
+    todate = forms.DateField(widget=forms.DateInput(attrs={"type": "date"}), required=False)
+    category = forms.ModelMultipleChoiceField(queryset=Category.objects.all(), required=False)
+    CHOICES = (('category', 'category'), ('date', 'date'))
+    sort_descending = forms.BooleanField(required=False)
+    order_by = forms.ChoiceField(choices=CHOICES, required=False)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['name'].required = False
+
+
+
+
+
+
--- expenses/views.py
+++ expenses/views.py
@@ -1,5 +1,9 @@
+from django.contrib.admin import filters
+from django.db.models import Sum, Count, Max, Q
+from django.db.models.functions import ExtractYear, ExtractMonth
 from django.views.generic.list import ListView
 
+from . import models
 from .forms import ExpenseSearchForm
 from .models import Expense, Category
 from .reports import summary_per_category
@@ -7,7 +11,7 @@ from .reports import summary_per_category
 
 class ExpenseListView(ListView):
     model = Expense
-    paginate_by = 5
+    paginate_by = 100  # how may items per page
 
     def get_context_data(self, *, object_list=None, **kwargs):
         queryset = object_list if object_list is not None else self.object_list
@@ -15,16 +19,54 @@ class ExpenseListView(ListView):
         form = ExpenseSearchForm(self.request.GET)
         if form.is_valid():
             name = form.cleaned_data.get('name', '').strip()
+            fromdate = form.cleaned_data.get('fromdate', '')
+            todate = form.cleaned_data.get('todate', '')
+            category = form.cleaned_data.get('category', '')
+            order_by = form.cleaned_data.get('order_by', '')
+            sort_descending = form.cleaned_data.get('sort_descending', '')
+
+            filters = {}
+
             if name:
-                queryset = queryset.filter(name__icontains=name)
+                filters['name__icontains'] = name
+            if fromdate and todate:
+                filters['date__range'] = [fromdate, todate]
+            if category:
+                filters['category__in'] = category
+
+            queryset = queryset.filter(**filters)
+
+            if order_by:
+                if sort_descending:
+                    queryset = queryset.order_by(order_by).reverse()
+                else:
+                    queryset = queryset.order_by(order_by)
+
+            total_amount_spent = queryset.aggregate(Sum('amount'))
+
+            total_summary_per_year_month = queryset.annotate(year=ExtractYear('date'),
+                                                             month=ExtractMonth('date')). \
+                values('year', 'month').annotate(sum=Sum('amount')).order_by('year', 'month')
 
         return super().get_context_data(
             form=form,
             object_list=queryset,
             summary_per_category=summary_per_category(queryset),
-            **kwargs)
+            total_amount_spent=total_amount_spent,
+            total_summary_per_year_month=total_summary_per_year_month,
+            **kwargs
+        )
+
 
 class CategoryListView(ListView):
     model = Category
     paginate_by = 5
 
+    def get_context_data(self, **kwargs):
+        query_set = [(c, Expense.objects.filter(category=c.id).count()) for c in Category.objects.all()]
+        query_set = dict(query_set)
+
+        return super().get_context_data(
+            query_set=query_set,
+            **kwargs
+        )
